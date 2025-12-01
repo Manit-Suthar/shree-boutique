8c595b01c31a1c7f76905ae15cd7ecaf64e2df6a
Manit_Suthar <manitsuthar2007@gmail.com>
2025-12-01 11:28:41 +0530

    images trial

--- sanity/lib/client.js
+++ sanity/lib/client.js
@@ -3,6 +3,6 @@ import { createClient } from "next-sanity";
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION,
-  useCdn: true,
+  apiVersion: "2023-05-03",
+  useCdn: false,
 });
